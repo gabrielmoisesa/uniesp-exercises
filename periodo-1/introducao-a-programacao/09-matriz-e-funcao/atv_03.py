2608c0c022b1623f16e4eb211fae963b2f876adb
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-05-21 00:33:35 -0300

    refactor(09-matriz-e-funcao): rename matrix max function

--- periodo-1/introducao-a-programacao/09-matriz-e-funcao/atv_03.py
+++ periodo-1/introducao-a-programacao/09-matriz-e-funcao/atv_03.py
@@ -1,6 +1,6 @@
 from atv_01 import create_matrix
 
-def greatest_matrix_value(matrix):
+def get_matrix_max(matrix):
   greatest_number = matrix[0][0]
 
   for l in matrix:
@@ -13,6 +13,6 @@ def greatest_matrix_value(matrix):
 
 if __name__ == "__main__":
   matriz = create_matrix(3, 3)
-  maior_valor = greatest_matrix_value(matriz)
+  maior_valor = get_matrix_max(matriz)
 
   print(f"O maior valor da matriz é {maior_valor}")
