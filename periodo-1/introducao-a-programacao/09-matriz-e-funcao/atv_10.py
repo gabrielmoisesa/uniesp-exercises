9ca394ad620d3934c8d86e16222f448b00586ee9
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-05-22 00:39:41 -0300

    feat(09-matriz-e-funcao): atv_10

--- periodo-1/introducao-a-programacao/09-matriz-e-funcao/atv_10.py
+++ periodo-1/introducao-a-programacao/09-matriz-e-funcao/atv_10.py
@@ -1,6 +1,8 @@
-from atv_01 import create_matrix
+from atv_01 import create_matrix, print_matrix_table
 
 matriz = create_matrix(3, 3)
+print_matrix_table(matriz)
 
+print("Somas:")
 for l in matriz:
   print(sum(l))
