2ac9c733a338c2063136ddbe1688af70d0c0a844
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-05-22 01:00:06 -0300

    feat(09-matriz-e-funcao): atv_19

---
+++ periodo-1/introducao-a-programacao/09-matriz-e-funcao/atv_19.py
@@ -0,0 +1,8 @@
+def calcular_media(nota1, nota2):
+  media = (nota1 + nota2) / 2
+  print("A média é", media)
+
+n1 = float(input("Digite a primeira nota:"))
+n2 = float(input("Digite a segunda nota:"))
+
+calcular_media(n1, n2)
