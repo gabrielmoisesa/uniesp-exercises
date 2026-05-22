77ef6b392147f4095a97228051e223d3b6be3379
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-05-22 00:33:10 -0300

    feat(09-matriz-e-funcao): atv_09

--- periodo-1/introducao-a-programacao/09-matriz-e-funcao/atv_09.py
+++ periodo-1/introducao-a-programacao/09-matriz-e-funcao/atv_09.py
@@ -1,5 +1,22 @@
 from atv_01 import create_matrix, print_matrix_table
 
+def get_students_average(students_matrix):
+  students_average = []
+
+  for student in students_matrix:
+    grades_sum = 0
+    average = 0
+
+    for grade in student:
+      grades_sum += grade
+
+    average = grades_sum / len(student)
+    students_average.append(average)
+  
+  return students_average
+
 if __name__ == "__main__":
   matriz = create_matrix(4, 3, float)
+
   print_matrix_table(matriz)
+  print("Média dos alunos:", get_students_average(matriz))
