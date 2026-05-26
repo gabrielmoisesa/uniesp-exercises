02f1ad21ac197a1645dbe9c153531b7df83a3a15
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-05-26 00:44:54 -0300

    feat(lista-final): atv_01

--- periodo-1/introducao-a-programacao/lista-final-exercicio/atv_01.py
+++ periodo-1/introducao-a-programacao/lista-final-exercicio/atv_01.py
@@ -45,5 +45,23 @@ def get_above_average(numbers, average):
   return above_average
 
 if __name__ == "__main__":
-  notas = create_list(5, float)
-  print(get_above_average(notas, 7))
+  notas_alunos = create_list(10, float)
+  media_turma = get_average(notas_alunos)
+  maior_nota = get_biggest_number(notas_alunos)
+  menor_nota = get_lowest_number(notas_alunos)
+  qtd_acima_da_media = len(get_above_average(notas_alunos, media_turma))
+  
+  qtd_abaixo_de_cinco = 0
+
+  for n in notas_alunos:
+    if n < 5:
+      qtd_abaixo_de_cinco += 1
+  
+  print(f"""
+  Média da turma: {media_turma}
+  Maior nota: {maior_nota}
+  Menor nota: {menor_nota}
+  Quantidade de alunos acima da média: {qtd_acima_da_media}
+  Quantidade de alunos abaixo de 5: {qtd_abaixo_de_cinco}
+  """
+  )
