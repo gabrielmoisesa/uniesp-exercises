6442de8b24e774c2678aa2bd15dcab31259e16f7
Gabriel Alves <gabrielmoisesalves18@gmail.com>
2026-08-18 22:56:52 +0000

    feat: atv01 frontend ii

---
+++ periodo-2/front-end-ii/ted-1-1/atv_01.py
@@ -0,0 +1,41 @@
+alturasFeminino = []
+alturasMasculino = []
+
+for i in range(4):
+    genero = input("\nInforme o gênero: ")
+    altura = float(input("Informe a altura: "))
+
+    if (genero == "feminino"):
+        alturasFeminino.append(altura)
+    elif (genero == "masculino"):
+        alturasMasculino.append(altura)
+    else:
+        print("Gênero inválido!")
+        continue
+
+maiorAlturaFeminino = max(alturasFeminino)
+maiorAlturaMasculino = max(alturasMasculino)
+maiorAlturaTotal = 0
+
+if (maiorAlturaFeminino < maiorAlturaMasculino):
+    maiorAlturaTotal = maiorAlturaMasculino
+else:
+    maiorAlturaTotal = maiorAlturaFeminino
+
+menorAlturaFeminino = min(alturasFeminino)
+menorAlturaMasculino = min(alturasMasculino)
+menorAlturaTotal = 0
+
+if (menorAlturaFeminino < menorAlturaMasculino):
+    menorAlturaTotal = menorAlturaFeminino
+else:
+    menorAlturaTotal = menorAlturaMasculino
+
+mediaAlturaMasculino = (sum(alturasMasculino)) / len(alturasMasculino)
+
+print(f"\nMaior altura do grupo: {maiorAlturaTotal}")
+print(f"Menor altura do grupo: {menorAlturaTotal}")
+
+print(f"Média de altura do gênero Masculino: {mediaAlturaMasculino}")
+
+print(f"Número de pessoas do gênero Feminino: {len(alturasFeminino)}")
