4d493f47af6a215cceb6ce100db895750b6dc1c1
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-08-26 02:35:09 -0300

    feat(poo-aula3): ex08

---
+++ periodo-2/linguagem-de-programacao-i/poo-aula3/src/Ex08_CadastroVariosTipos.java
@@ -0,0 +1,23 @@
+public class Ex08_CadastroVariosTipos {
+  public static void main(String[] args) {
+    byte idade = 22;
+    short ano = 2026;
+    int quantidade = 10;
+    long matricula = 2026111890104L;
+    float altura = 1.69f;
+    double preco = 9.99;
+    boolean verdadeiro = true;
+    char sexo = 'M';
+    String nome = "Gabriel";
+
+    System.out.println("idade (byte): " + idade);
+    System.out.println("ano (short): " + ano);
+    System.out.println("quantidade (int): " + quantidade);
+    System.out.println("matricula (long): " + matricula);
+    System.out.println("altura (float): " + altura);
+    System.out.println("preco (double): " + preco);
+    System.out.println("verdadeiro (boolean): " + verdadeiro);
+    System.out.println("sexo (char): " + sexo);
+    System.out.println("nome (String): " + nome);
+  }
+}
