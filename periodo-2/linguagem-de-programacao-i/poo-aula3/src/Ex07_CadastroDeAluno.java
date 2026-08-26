7fa548863ed23d8733852e1d9f18b2f1d4ad06db
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-08-26 02:25:54 -0300

    feat(poo-aula3): ex07

---
+++ periodo-2/linguagem-de-programacao-i/poo-aula3/src/Ex07_CadastroDeAluno.java
@@ -0,0 +1,15 @@
+public class Ex07_CadastroDeAluno {
+  public static void main(String[] args) {
+    String nome = "Gabriel Moises Alves";
+    int idade = 22;
+    long matricula = 2026111890104L;
+    String curso = "Ciências da Computação";
+
+    System.out.println("================================\nCadastro do Aluno\n================================");
+    System.out.println("Nome: " + nome);
+    System.out.println("Idade: " + idade);
+    System.out.println("Matrícula: " + matricula);
+    System.out.println("Curso: " + curso);
+    System.out.println("================================");
+  }
+}
