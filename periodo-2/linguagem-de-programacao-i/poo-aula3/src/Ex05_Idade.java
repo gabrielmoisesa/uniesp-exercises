75ec3a4644971ba959c186b52a07d22c0ade67f3
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-08-26 02:02:52 -0300

    fix(poo-aula3): scanner usages without closing

--- periodo-2/linguagem-de-programacao-i/poo-aula3/src/Ex05_Idade.java
+++ periodo-2/linguagem-de-programacao-i/poo-aula3/src/Ex05_Idade.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 
 public class Ex05_Idade {
   public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
+    try (Scanner input = new Scanner(System.in)) {
+      System.out.println("Digite o seu nome:");
+      String nome = input.nextLine();
 
-    System.out.println("Digite o seu nome:");
-    String nome = input.nextLine();
+      System.out.println("Digite a sua idade:");
+      int idade = input.nextInt();
 
-    System.out.println("Digite a sua idade:");
-    int idade = input.nextInt();
-
-    System.out.println("\nOlá, " + nome + "!");
-    System.out.println("Você tem " + idade + " anos");
+      System.out.println("\nOlá, " + nome + "!");
+      System.out.println("Você tem " + idade + " anos");
+    }
   }
 }
