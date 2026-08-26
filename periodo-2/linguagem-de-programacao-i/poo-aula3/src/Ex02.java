6f2571bb392422d5b2f891d8c5f6d8e7f89546d9
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-08-26 01:34:47 -0300

    feat(poo-aula3): ex02

---
+++ periodo-2/linguagem-de-programacao-i/poo-aula3/src/Ex02.java
@@ -0,0 +1,13 @@
+public class Ex02 {
+  public static void main(String[] args) {
+    String nome = "Gabriel";
+    int idade = 22;
+    String cidade = "João Pessoa";
+    String curso = "Ciências da Computação";
+
+    System.out.println("Nome: " + nome);
+    System.out.println("Idade: " + idade);
+    System.out.println("Cidade: " + cidade);
+    System.out.println("Curso: " + curso);
+  }
+}
