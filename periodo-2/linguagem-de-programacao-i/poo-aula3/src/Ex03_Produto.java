d01ce663067631dba8e130e8b37911114a05769f
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-08-26 01:51:06 -0300

    feat(poo-aula3): ex03

---
+++ periodo-2/linguagem-de-programacao-i/poo-aula3/src/Ex03_Produto.java
@@ -0,0 +1,11 @@
+public class Ex03_Produto {
+  public static void main(String[] args) {
+    String nomeDoProduto = "Caneta";
+    double preco = 4.99;
+    int quantidade = 100;
+
+    System.out.println("Nome do produto: " + nomeDoProduto);
+    System.out.println("Preço: R$" + preco);
+    System.out.println("Quantidade: " + quantidade);
+  }
+}
