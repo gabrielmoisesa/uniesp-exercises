566300ffe1d860a578900084152f2650d0894945
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-08-26 01:26:55 -0300

    feat: init poo-aula3 project and ex01

---
+++ periodo-2/linguagem-de-programacao-i/poo-aula3/src/Ex01.java
@@ -0,0 +1,11 @@
+public class Ex01 {
+  public static void main(String[] args) {
+    System.out.println("================================\n" + //
+        "MEU PRIMEIRO PROGRAMA\n" + //
+        "================================\n" + //
+        "Nome: Gabriel Moises Alves\n" + //
+        "Curso: Ciências da Computação\n" + //
+        "Linguagem: Java\n" + //
+        "================================");
+  }
+}
