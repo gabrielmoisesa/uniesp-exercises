1a866a2dac99fdb96758f15e183f06cf43deebdb
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-08-26 01:51:19 -0300

    feat(poo-aula3): ex04

---
+++ periodo-2/linguagem-de-programacao-i/poo-aula3/src/Ex04_NomeDoUsuario.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ex04_NomeDoUsuario {
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Digite o seu nome:");
+    String nomeUsuario = input.nextLine();
+
+    System.out.println("Olá, " + nomeUsuario + "!");
+    System.out.println("Seja bem-vindo ao Java!");
+  }
+}
