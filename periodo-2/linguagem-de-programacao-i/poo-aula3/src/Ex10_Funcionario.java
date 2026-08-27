e774c3466126efc84d0a642f26c6c849060bea9b
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-08-27 00:48:44 -0300

    feat(poo-aula3): ex10

---
+++ periodo-2/linguagem-de-programacao-i/poo-aula3/src/Ex10_Funcionario.java
@@ -0,0 +1,15 @@
+public class Ex10_Funcionario {
+  public static void main(String[] args) {
+    String nome = "Gabriel";
+    int idade = 22;
+    double salario = 0.0;
+    boolean ativo = false;
+    char sexo = 'M';
+
+    System.out.println("Nome: " + nome);
+    System.out.println("Idade: " + idade);
+    System.out.println("Salário: R$" + salario);
+    System.out.println("Ativo: " + ativo);
+    System.out.println("Sexo: " + sexo);
+  }
+}
