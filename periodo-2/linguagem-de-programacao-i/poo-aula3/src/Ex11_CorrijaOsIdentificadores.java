f9585ad4f252751a675a2e4caf0a50cfc103f489
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-08-27 00:52:29 -0300

    feat(poo-aula3): ex11

---
+++ periodo-2/linguagem-de-programacao-i/poo-aula3/src/Ex11_CorrijaOsIdentificadores.java
@@ -0,0 +1,17 @@
+public class Ex11_CorrijaOsIdentificadores {
+  public static void main(String[] args) {
+    // O código abaixo possui identificadores inválidos. Corrija-os para que o
+    // programa possa ser compilado:
+    // int 1idade;
+    // String nome completo;
+    // double #salario;
+    // int 2numero;
+    // String %cidade;
+
+    int idade;
+    String nomeCompleto;
+    double salario;
+    int numero;
+    String cidade;
+  }
+}
