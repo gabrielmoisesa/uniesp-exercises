6dea5ced509038efaeaf2e39132595ed2e767f30
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-08-27 00:43:29 -0300

    feat(poo-aula3): ex09

---
+++ periodo-2/linguagem-de-programacao-i/poo-aula3/src/Ex09_ProdutoCompleto.java
@@ -0,0 +1,20 @@
+public class Ex09_ProdutoCompleto {
+  public static void main(String[] args) {
+    String nome = "Caneta";
+    Long codigo = 18574632L;
+    int quantidade = 150;
+    double preco = 5.79;
+    boolean disponivel = true;
+    char categoria = 'M';
+
+    System.out.println("=======================");
+    System.out.println("Informações do produto");
+    System.out.println("=======================");
+    System.out.println("Nome: " + nome);
+    System.out.println("Código: " + codigo);
+    System.out.println("Quantidade: " + quantidade);
+    System.out.println("Preço: " + preco);
+    System.out.println("Disponível: " + disponivel);
+    System.out.println("Categoria: " + categoria);
+  }
+}
