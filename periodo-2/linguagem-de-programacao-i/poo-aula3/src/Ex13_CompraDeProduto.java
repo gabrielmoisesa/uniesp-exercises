e12245a4f3b8514e08a1449c5eb794d4e73eb9e2
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-08-27 01:06:50 -0300

    feat(poo-aula3): ex13

---
+++ periodo-2/linguagem-de-programacao-i/poo-aula3/src/Ex13_CompraDeProduto.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ex13_CompraDeProduto {
+  public static void main(String[] args) {
+    try (Scanner input = new Scanner(System.in)) {
+      System.out.println("Digite o nome do produto:");
+      String nome = input.nextLine();
+
+      System.out.println("Digite o preço do produto:");
+      double preco = input.nextDouble();
+
+      System.out.println("Digite a quantidade do produto:");
+      int quantidade = input.nextInt();
+
+      System.out.println("================");
+      System.out.println("Resumo da Compra");
+      System.out.println("================");
+      System.out.println("Produto: " + nome);
+      System.out.println("Preço: R$ " + preco);
+      System.out.println("Quantidade: " + quantidade);
+      System.out.println("Total: R$ " + preco * quantidade);
+    }
+  }
+
+}
