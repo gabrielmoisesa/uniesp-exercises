7c5fe8487407206b1a1cef1ed96b0b205244790a
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-08-27 00:59:22 -0300

    feat(poo-aula3): ex12

---
+++ periodo-2/linguagem-de-programacao-i/poo-aula3/src/Ex12_CrieSeusIdentificadores.java
@@ -0,0 +1,12 @@
+public class Ex12_CrieSeusIdentificadores {
+  boolean _comecaComUnderline = true;
+  int tenhoNumero4 = 4;
+  String snake_case = "exemplo_de_snake_case_com_underline";
+  double altura = 1.75;
+  char letra = 'A';
+  long idUsuario = 123456789L;
+  float velocidade = 45.5f;
+  String nomeCompleto = "Gabriel Moises Alves";
+  int idade = 25;
+  boolean ativo = false;
+}
